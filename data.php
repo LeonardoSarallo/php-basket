d66a53e87a82a41a46e80235a8c4da56405b3201
LeonardoSarallo <leo.sarallo@gmail.com>
2019-01-24 17:18:57 +0100

    provaecho

--- data.php
+++ data.php
@@ -1,34 +1,36 @@
 <?php
-  $letter = " " ;
-  $number = " ";
-  for ($i = 0; $i < 3; $i++) {
-    $number .= rand(0, 10);
-    $letter .= chr(64+rand(0,26));
+  function letterandnumber() {
+    $letter = " " ;
+    $number = " ";
+    for ($i = 0; $i < 3; $i++) {
+      $number .= rand(0, 10);
+      $letter .= chr(64+rand(0,25));
 
 
+    }
+    return $letter . $number;
   }
+
   // var_dump($number);
   // var_dump($letter);
 
-  $numeropunti =  rand(10, 40);
-  $numerorimbalzi = rand(5, 20);
-  $falli =  rand(5, 15);
-  $percentualeduepunti = rand(70, 80);
-  $percentualetrepunti = rand(20, 40);
 
+  $giocatori = [];
   for ($i=0; $i <100 ; $i++) {
     $giocatore= [
-      'codice giocatore'=> $letter . $number,
-      'numero punti'=> $numeropunti,
-      'numero rimbalzi'=> $numerorimbalzi,
-      'falli'=> $falli,
-      'percentuale due punti'=> $percentualeduepunti,
-      'percentuale tre punti'=> $percentualetrepunti,
+      'codice giocatore'=> letterandnumber(),
+      'numero punti'=> rand(10, 40),
+      'numero rimbalzi'=> rand(5, 20),
+      'falli'=> rand(5, 15),
+      'percentuale due punti'=> rand(70, 80),
+      'percentuale tre punti'=> rand(20, 40),
 
 
 
 
     ];
+    array_push($giocatori, $giocatore);
   }
-  var_dump($giocatore);
+  // var_dump($giocatori);
+
 ?>
--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 
       include 'data.php';
     ?>
+    <ul>
+      <?php foreach ($giocatori as $key => $data) { ?>
+          
+          <li><?php echo  $data['codice giocatore'] ?></li>   <?php echo "<br>";  ?>
+          <li><?php echo  $data['numero punti'] ?></li>   <?php echo "<br>";  ?>
+          <li><?php echo  $data['numero rimbalzi'] ?></li>   <?php echo "<br>";  ?>
+          <li><?php echo  $data['falli'] ?></li>   <?php echo "<br>";  ?>
+          <li><?php echo  $data['percentuale due punti'] ?></li>   <?php echo "<br>";  ?>
+          <li><?php echo  $data['percentuale tre punti'] ?></li>   <?php echo "<br>";  ?>
+
+
+       <?php }  ?>
+    </ul>
+
 
   </body>
 </html>
