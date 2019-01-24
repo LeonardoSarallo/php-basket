ae0e70e936fa76002a525acd125a08d3b7d251a7
LeonardoSarallo <leo.sarallo@gmail.com>
2019-01-24 17:26:53 +0100

    fineesercizio

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
     <title></title>
   </head>
   <body>
@@ -10,14 +11,14 @@
       include 'data.php';
     ?>
     <ul>
-      <?php foreach ($giocatori as $key => $data) { ?>
-          
-          <li><?php echo  $data['codice giocatore'] ?></li>   <?php echo "<br>";  ?>
-          <li><?php echo  $data['numero punti'] ?></li>   <?php echo "<br>";  ?>
-          <li><?php echo  $data['numero rimbalzi'] ?></li>   <?php echo "<br>";  ?>
-          <li><?php echo  $data['falli'] ?></li>   <?php echo "<br>";  ?>
-          <li><?php echo  $data['percentuale due punti'] ?></li>   <?php echo "<br>";  ?>
-          <li><?php echo  $data['percentuale tre punti'] ?></li>   <?php echo "<br>";  ?>
+      <?php foreach ($giocatori as $data) { ?>
+
+          <li>Codice giocatore: <?php echo  $data['codice giocatore'] ?></li>
+          <li>Numero punti:<?php echo  $data['numero punti'] ?></li>
+          <li>Numero rimbalzi: <?php echo  $data['numero rimbalzi'] ?></li>
+          <li>Falli: <?php echo  $data['falli'] ?></li>   <?php echo "<br>";  ?>
+          <li>Percentuale due punti: <?php echo  $data['percentuale due punti'] ?></li>   
+          <li>Percentuale tre punti: <?php echo  $data['percentuale tre punti'] ?></li>
 
 
        <?php }  ?>
